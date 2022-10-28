4dc55fbe680dc4a866c4bfb8178c0f72d86896e6
“zzy2210” <670379628@qq.com>
2022-10-28 22:43:25 +0800

    chore/stack:implement linked stack #1

--- stacks/linked_stack/linked_stack.go
+++ stacks/linked_stack/linked_stack.go
@@ -1,3 +1,45 @@
 package linkedstack
 
-// TODO : implement linked stack by link list
+//  以头节点为栈顶
+
+type LinkedStack struct {
+	ChainNode *chainNode
+	StackSize int
+}
+
+type chainNode struct {
+	Val  interface{}
+	Next *chainNode
+}
+
+func (s *LinkedStack) Empty() bool {
+	return s.StackSize == 0
+}
+
+func (s *LinkedStack) Size() int {
+	return s.StackSize
+}
+
+func (s *LinkedStack) Top() (val interface{}, ok bool) {
+	if s.Empty() {
+		return nil, false
+	}
+	return s.ChainNode.Val, true
+}
+
+func (s *LinkedStack) Pop() (val interface{}, ok bool) {
+	if s.Empty() {
+		return nil, false
+	}
+	val = s.ChainNode.Val
+	s.ChainNode = s.ChainNode.Next
+	return val, true
+}
+
+func (s *LinkedStack) Push(val interface{}) (ok bool) {
+	s.ChainNode.Next = &chainNode{
+		Val:  val,
+		Next: s.ChainNode,
+	}
+	return true
+}
