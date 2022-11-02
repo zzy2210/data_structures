4a3f2fb38ad174b2f0a865c42f04fabf4b54fc33
“zzy2210” <670379628@qq.com>
2022-11-02 22:19:04 +0800

    chore/queue:implement array queue test #7

--- queue/array_queue/array_queue_test.go
+++ queue/array_queue/array_queue_test.go
@@ -24,3 +24,62 @@ func TestPush(t *testing.T) {
 		t.Errorf("Got %v expected %v", get, 4)
 	}
 }
+
+func TestPop(t *testing.T) {
+	q := arrayqueue.New()
+	get, ok := q.Pop()
+	if ok {
+		t.Errorf("Got %v expect %v", ok, false)
+	}
+	q.Push(1)
+	q.Push(2)
+	q.Push(3)
+	get, ok = q.Pop()
+	if !ok {
+		t.Errorf("Got %v expect %v", ok, true)
+	}
+	if get != 1 {
+		t.Errorf("Got %v expect %v", get, 1)
+	}
+	if get := q.Size(); get != 2 {
+		t.Errorf("Got %v expect %v", get, 2)
+	}
+	if get := q.QueueFront; get != 1 {
+		t.Errorf("Got %v expect %v", get, 1)
+	}
+}
+
+func TestFront(t *testing.T) {
+	q := arrayqueue.New()
+	_, ok := q.Front()
+	if ok {
+		t.Errorf("Got %v expect %v", ok, false)
+	}
+	q.Push(1)
+	q.Push(2)
+	q.Push(3)
+	get, ok := q.Front()
+	if !ok {
+		t.Errorf("Got %v expect %v", ok, true)
+	}
+	if get.(int) != 1 {
+		t.Errorf("Got %v expect %v", get, 1)
+	}
+}
+
+func TestBack(t *testing.T) {
+	q := arrayqueue.New()
+	if _, ok := q.Back(); ok {
+		t.Errorf("Got %v expect %v", ok, 1)
+	}
+	q.Push(1)
+	q.Push(2)
+	q.Push(3)
+	get, ok := q.Back()
+	if !ok {
+		t.Errorf("Got %v expect %v", ok, true)
+	}
+	if get.(int) != 3 {
+		t.Errorf("Got %v expect %v", get, 3)
+	}
+}
